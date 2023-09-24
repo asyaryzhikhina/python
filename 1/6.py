d469ba0765bd405b47a2b9419bd1f14c27dffb95
asyaryzhikhina <142721089+asyaryzhikhina@users.noreply.github.com>
2023-09-24 17:33:38 +0300

    Create 6.py

---
+++ 1/6.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = np.arange(-2, 2.01, 0.001)
+c = 100
+b = 0.25
+a = 5
+y1 = np.array([])
+y = np.array([])
+for x in x1:
+    for i in range(c):
+        y = np.append(y, np.cos(np.pi*x*a**i)*b**i)
+    y2 = np.sum(y)
+    y1 = np.append(y1, y2)
+    y = np.array([])
