3f949e110ee4ce51f4fb29f00aed0d7e76f07ce7
asyaryzhikhina <142721089+asyaryzhikhina@users.noreply.github.com>
2023-10-01 13:39:01 +0300

    Update 9.py

--- 2/9.py
+++ 2/9.py
@@ -1,2 +1,24 @@
+import turtle                                         
 import numpy as np
-import turtle
+
+turtle.shape('turtle')                                     
+rad = 20                                                
+x = 1                                                 
+n = 3                                                 
+turtle.up()                                                
+turtle.goto(rad, 0)                                          
+turtle.down()  
+
+def polygon(x):                                       
+    for x in range (1, n+1):                                     
+        turtle.left((180 - 360 / n) / 2)                   
+        turtle.left(360 / n)                               
+        turtle.forward(2 * rad * np.sin(np.pi/n))                                     
+        turtle.right((180 - 360 / n) / 2)                  
+for n in range (3,12):                                        
+    polygon(x)                                        
+    n += 1                                            
+    rad += 20                                           
+    turtle.up()                                            
+    turtle.goto(rad, 0)                                      
+    turtle.down()  
