f02e91d9d92e82bd41459f663d72cfaa41082569
asyaryzhikhina <142721089+asyaryzhikhina@users.noreply.github.com>
2023-11-23 08:15:20 +0300

    Create generate.py

---
+++ 7/generate.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def generate_matrix(n, grid_size=50):
+
+    grid = np.zeros((grid_size, grid_size))
+    indices = np.random.choice(grid_size*grid_size, n, replace=False)
+    np.put(grid, indices, 1)
+
+    return grid
+
+
+def save_matrix_to_file(matrix, filename):
+    np.savetxt(filename, matrix, fmt='%d', delimiter=',')
+
+
+def get_user_input():
+    return int(input("Количество единиц в матрице: "))
+
+
+n = get_user_input()
+
+matrix = generate_matrix(n)
+
+save_matrix_to_file(matrix, 'matrixforgame.txt')
